7ee39c8e9d136822d5cb6231c6290ebcac15e160
karamTahhan <kata07001@utb.vaxjo.se>
2024-10-03 14:12:24 +0200

    Add project files.

---
+++ Uppgift 2_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace uppgift2_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många dagar tänker du köra bilen?");
+            int dagar =int.Parse(Console.ReadLine());
+            int dag1 = 300;
+            int överst = 500;
+            Console.WriteLine("hur många Kilometer tänker du köra?");
+            int Km =int.Parse(Console.ReadLine());
+            int sum= dag1 + dagar*överst +Km;
+            Console.WriteLine(sum);
+
+               
+
+
+
+
+
+
+
+        }
+
+
+    }
+        
+
+}
